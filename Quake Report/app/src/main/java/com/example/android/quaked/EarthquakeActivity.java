cd1f2b2db9b0cb4edaf8f99eeb695c4747e65d2f
murdens <sonya.murden@gmail.com>
2020-04-16 23:04:15 +0100

    refactored Quake Report project to AndroidX

--- Quake Report/app/src/main/java/com/example/android/quaked/EarthquakeActivity.java
+++ Quake Report/app/src/main/java/com/example/android/quaked/EarthquakeActivity.java
@@ -19,7 +19,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
