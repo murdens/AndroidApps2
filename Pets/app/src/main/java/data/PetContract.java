38065a91756f7a74da8ccbcbf0bb29546431b22e
murdens <sonya.murden@gmail.com>
2020-04-27 18:55:29 +0100

    additions for readme, plus init commit for SQLite project - pets

--- Pets/app/src/main/java/data/PetContract.java
+++ Pets/app/src/main/java/data/PetContract.java
@@ -1,4 +1,26 @@
 package data;
 
-public class PetContract {
+import android.provider.BaseColumns;
+
+public final class PetContract {
+    private PetContract() {}
+
+    public static class PetEntry implements BaseColumns {
+        public static final String TABLE_NAME = "pets";
+        public static final String _ID = BaseColumns._ID;
+        public static final String COLUMN_PET_NAME = "name";
+        public static final String COLUMN_PET_BREED = "breed";
+        public static final String COLUMN_PET_GENDER = "gender";
+        public static final String COLUMN_PET_WEIGHT = "weight";
+
+        /**
+         * Possible values for the gender.
+         */
+        public static final int GENDER_MALE = 1;
+        public static final int GENDER_FEMALE = 2;
+        public static final int GENDER_UNKNOWN = 0;
+    }
+
 }
+
+
--- Pets/app/src/main/java/data/PetDbHelper.java
+++ Pets/app/src/main/java/data/PetDbHelper.java
@@ -1,4 +1,39 @@
 package data;
 
-public class PetDbHelper {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import data.PetContract.PetEntry;
+import androidx.annotation.Nullable;
+
+public class PetDbHelper extends SQLiteOpenHelper {
+    // If you change the database schema, you must increment the database version.
+    public static final int DATABASE_VERSION = 1;
+
+    public static final String DATABASE_NAME = "Shelter.db";
+
+    public PetDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String SQL_CREATE_ENTRIES =
+                "CREATE TABLE " + PetEntry.TABLE_NAME + " ("
+                        + PetEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                        + PetEntry.COLUMN_PET_NAME + " TEXT NOT NULL,"
+                        + PetEntry.COLUMN_PET_BREED + " TEXT,"
+                        + PetEntry.COLUMN_PET_GENDER + " INTEGER NOT NULL,"
+                        + PetEntry.COLUMN_PET_WEIGHT + " INTEGER NOT NULL DEFAULT 0);";
+
+        db.execSQL(SQL_CREATE_ENTRIES);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        String SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS " + PetEntry.TABLE_NAME;
+        db.execSQL(SQL_DELETE_ENTRIES);
+        onCreate(db);
+    }
 }
+
