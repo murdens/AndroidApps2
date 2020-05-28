226e1bec8fe5255b890123b94f7eb23e312f5243
murdens <sonya.murden@gmail.com>
2020-05-28 23:26:53 +0100

    init app using room and live data model

--- NoDo/app/src/main/java/com/example/nodo/data/NoDoDao.java
+++ NoDo/app/src/main/java/com/example/nodo/data/NoDoDao.java
@@ -1,4 +1,31 @@
 package com.example.nodo.data;
 
+import androidx.lifecycle.LifecycleService;
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import com.example.nodo.model.NoDO;
+
+import java.util.List;
+
+@Dao
 public interface NoDoDao {
+
+    //CRUD
+    @Insert
+    void insert(NoDO noDO);
+
+    @Query("DELETE FROM nodo_table")
+    void deleteAll();
+
+    @Query("DELETE FROM nodo_table WHERE id = :id")
+    int deleteNoDo(int id);
+
+    @Query("UPDATE nodo_table SET nodo_col = :noDoText WHERE id= :id")
+    int updateNoDo(int id, String noDoText);
+
+    @Query("SELECT * FROM nodo_table ORDER BY nodo_col DESC")
+    LiveData<List<NoDO>> getAllNoDos();
 }
--- NoDo/app/src/main/java/com/example/nodo/model/NoDO.java
+++ NoDo/app/src/main/java/com/example/nodo/model/NoDO.java
@@ -1,4 +1,39 @@
 package com.example.nodo.model;
 
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "nodo_table")
 public class NoDO {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    @NonNull
+    @ColumnInfo(name = "nodo_col")
+    private String noDo;
+
+
+    public NoDO(@NonNull String noDo){
+        this.noDo = noDo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @NonNull
+    public String getNoDo() {
+        return noDo;
+    }
+
+    public void setNoDo(@NonNull String noDo) {
+        this.noDo = noDo;
+    }
 }
